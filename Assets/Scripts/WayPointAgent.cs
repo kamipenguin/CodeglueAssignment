0d52fd993f6aaaa495ee0fc9cbbea4bb6691d52f
Roxanne van Dam <roxanne.vandam@outlook.com>
2020-09-22 01:25:31 +0200

    setting up scene and implemented moving enemy

---
+++ Assets/Scripts/WayPointAgent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointAgent : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _wayPoints = new List<Transform>();
+    [SerializeField]
+    private float _moveSpeed = 5;
+    [SerializeField]
+    private float _waitTime = 0.5f;
+    [SerializeField]
+    private float _goalReachedDistance = 0.2f;
+
+    private int _currentIndex = 0;
+    private bool _hasReachedDestination;
+
+    private void Update()
+    {
+        if (!_hasReachedDestination)
+        {
+            Vector3 currentPosition = gameObject.transform.position;
+            Vector3 goalPosition = _wayPoints[_currentIndex].position;
+            gameObject.transform.position = Vector3.MoveTowards(currentPosition, goalPosition, _moveSpeed);
+
+            CheckDestinationReached(currentPosition, goalPosition);
+        }
+    }
+
+    private void CheckDestinationReached(Vector3 currentPos, Vector3 goalPos)
+    {
+        if (Vector3.Distance(currentPos, goalPos) <= _goalReachedDistance)
+        {
+            _hasReachedDestination = true;
+            StartCoroutine(WaitAndTurn());
+        }
+    }
+
+    private IEnumerator WaitAndTurn()
+    {
+        yield return new WaitForSeconds(_waitTime);
+
+        if (_currentIndex == 0)
+            _currentIndex = 1;
+        else
+            _currentIndex = 0;
+
+        _hasReachedDestination = false;
+    }
+}
