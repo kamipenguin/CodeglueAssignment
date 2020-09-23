334783de24fbc34b01f346cc5470914620f3b400
Roxanne van Dam <roxanne.vandam@outlook.com>
2020-09-23 21:03:58 +0200

    spirit shit

---
+++ Assets/Scripts/FreedSpirit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreedSpirit : MonoBehaviour
+{
+    private SphereCollider _sphereCollider;
+    private float _waitTime = 0.15f;
+
+    private void Awake()
+    {
+        _sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    private void Start()
+    {
+        Wait();
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(_waitTime);
+        _sphereCollider.isTrigger = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Spirit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spirit : MonoBehaviour
+{   
+    [SerializeField]
+    private GameObject _crawler;
+    [SerializeField]
+    private GameObject _freedSpiritPrefab;
+
+    private GameObject _parentObject;
+
+    private float _shootSpiritForce = 7.5f;
+
+    private void Awake()
+    {
+        _parentObject = gameObject.transform.parent.gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject freedSpirit = Instantiate(_freedSpiritPrefab, transform.position, Quaternion.identity);
+            Rigidbody freedSpiritRigidbody = freedSpirit.GetComponent<Rigidbody>();
+            freedSpiritRigidbody.AddForce(Vector3.up * _shootSpiritForce, ForceMode.Impulse);
+
+            Destroy(_parentObject);
+        }
+    }
+}
